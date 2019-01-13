ccb336b902e60a285476dd532e5cfd07769fa4d0
Nikolay Kolpakov <nik.vik.kolpakov@gmail.com>
2019-01-14 03:13:53 +0500

    Init

---
+++ app/src/main/java/com/afkapps/android/geoquiz/Question.java
@@ -0,0 +1,10 @@
+package com.afkapps.android.geoquiz;
+
+public class Question {
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int textResId, boolean answerTrue ) {
+
+    }
+}
