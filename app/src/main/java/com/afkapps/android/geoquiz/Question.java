8499ab40282113f632dc11a138a258e6ba3bc626
Nikolay Kolpakov <nik.vik.kolpakov@gmail.com>
2019-01-16 11:30:45 +0500

    Something's went wrong...

--- app/src/main/java/com/afkapps/android/geoquiz/Question.java
+++ app/src/main/java/com/afkapps/android/geoquiz/Question.java
@@ -4,7 +4,25 @@ public class Question {
     private int mTextResId;
     private boolean mAnswerTrue;
 
-    public Question(int textResId, boolean answerTrue ) {
-
+    public int getTextResId() {
+        return mTextResId;
     }
+
+    public void setTextResId(int pTextResId) {
+        mTextResId = pTextResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean pAnswerTrue) {
+        mAnswerTrue = pAnswerTrue;
+    }
+
+    public Question(int textResId, boolean answerTrue ) {
+        mTextResId = textResId;
+        mAnswerTrue = answerTrue;
+    }
+
 }
